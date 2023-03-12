a600bdce6dbd472138503a3662ddf91f10134bb4
Lucas Travessa <lucas.cos.travessa@gmail.com>
2023-03-12 11:06:47 -0300

    background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import sound from './assets/Sound.wav';
+import {useState} from 'react';
 
 function App() {
+  const [play, setPlay]= useState(0);
+
+  function Play(){
+    new Audio(sound).play()
+  }
+
   return (
       <div className='App'>
-
+        <header>
+          <span className='Title'></span>
+          <div className='Mute' onClick={Play}>{}</div>
+        </header>
       </div>
   );
 }
